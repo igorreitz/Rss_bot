2e8dcbd283ba6a45261b064ba44f2b44719bd386
igorreitz <rayts@ngs.ru>
2019-04-16 22:27:57 +0700

    Parse RSS & make prototype

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,38 +1,58 @@
+import com.sun.syndication.feed.synd.SyndEntry;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 
 public class Bot extends TelegramLongPollingBot {
+    public static List<SyndEntry> feedList = new ArrayList<>();
 
     /**
      * Метод для приема сообщений.
+     *
      * @param update Содержит сообщение от пользователя.
      */
     public void onUpdateReceived(Update update) {
-        System.out.println(update.getMessage().getFrom().getFirstName()+": "+update.getMessage().getText());
+        //System.out.println(update.getMessage().getFrom().getFirstName() + ": " + update.getMessage().getText());
         Message message = update.getMessage();
-        if(message!= null && message.hasText()){
-            if(message.getText().equals("/help")){
-                sendMsg(message,"Привет");
-            }
-            else{
-                sendMsg(message,"Я тест. робот");
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/start":
+                case "Далее":
+                    sendMsg(message, getFirstEntry());
+                    break;
+                case "Сначала":
+                    sendMsg(message, "Сначала");
+                    break;
+                case "Сначала*":
+                    sendMsg(message, "Сначала!");
+                    break;
+                default:
+                    sendMsg(message, "Неизвестная команда.");
+                    break;
             }
         }
     }
 
+    /**
+     * Метод для приема сообщений.
+     *
+     * @param updates Содержит список сообщений от пользователя.
+     */
     public void onUpdatesReceived(List<Update> updates) {
         updates.forEach(this::onUpdateReceived);
     }
 
     /**
      * Метод для настройки сообщения и его отправки.
-
+     *
      * @param s Строка, которую необходимот отправить в качестве сообщения.
      */
 
@@ -42,6 +62,7 @@ public class Bot extends TelegramLongPollingBot {
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(s);
+        setButtons(sendMessage, !s.equals("Далее"));
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
@@ -50,9 +71,9 @@ public class Bot extends TelegramLongPollingBot {
     }
 
 
-
     /**
      * Метод возвращает имя бота, указанное при регистрации.
+     *
      * @return имя бота
      */
     public String getBotUsername() {
@@ -61,9 +82,49 @@ public class Bot extends TelegramLongPollingBot {
 
     /**
      * Метод возвращает token бота для связи с сервером Telegram
+     *
      * @return token для бота
      */
     public String getBotToken() {
         return "";
+    }
+
+    public String getFirstEntry() {
+        String s = RSSParser.printEntry(feedList.get(feedList.size() - 1));
+        feedList.remove(feedList.size() - 1);
+        return s;
+    }
+
+    public synchronized void setButtons(SendMessage sendMessage, boolean hasNewEntry) {
+        // Создаем клавиуатуру
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        // Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        keyboardFirstRow.add(new KeyboardButton("Далее"));
+
+        // Вторая строчка клавиатуры
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Добавляем кнопки во вторую строчку клавиатуры
+        if (hasNewEntry) {
+            keyboardSecondRow.add(new KeyboardButton("Сначала*"));
+        } else {
+            keyboardSecondRow.add(new KeyboardButton("Сначала"));
+        }
+
+        // Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+
+        // и устанваливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,33 @@
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.getProperties().put( "proxySet", "true" );
-        System.getProperties().put( "socksProxyHost", "127.0.0.1" );
-        System.getProperties().put( "socksProxyPort", "9150" );
+        //Если провайдер блокирует доступ к телеграму, можно установить Tor-браузер,
+        //запустить его и раскомментировать строчки ниже:
+        //System.getProperties().put( "proxySet", "true" );
+        //System.getProperties().put( "socksProxyHost", "127.0.0.1" );
+        //System.getProperties().put( "socksProxyPort", "9150" );
+
+
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
-            telegramBotsApi.registerBot(new Bot());
-        } catch (TelegramApiRequestException e) {
+            Bot bot = new Bot();
+            telegramBotsApi.registerBot(bot);
+            RSSParser parser = new RSSParser();
+            //Bot.feedList.addAll(parser.parseFeed("https://awas1952.livejournal.com/data/rss").getEntries());
+            Bot.feedList.addAll(parser.parseFeed("https://news.mail.ru/rss/main/91/").getEntries());
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
+
     }
 }
